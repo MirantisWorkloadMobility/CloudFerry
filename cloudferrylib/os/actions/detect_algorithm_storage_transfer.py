0fa3e017a729260d14a00e3336ecd8c1b613599b
Maksym Iarmak <Maksym_Iarmak@symantec.com>
2014-11-07 15:15:32 +0200

    Fix actions
    - fix 'detect_algorithm_storage_transfer'
    - delete redundant 'create_new_volumes'

--- cloudferrylib/os/actions/detect_algorithm_storage_transfer.py
+++ cloudferrylib/os/actions/detect_algorithm_storage_transfer.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and#
 # limitations under the License.
 
+
 from cloudferrylib.base.action import action
 from cloudferrylib.utils import utils as utl
-__author__ = 'mirrorcoder'
 
 FILE_TO_FILE = 1
 FILE_TO_CEPH = 2
@@ -26,9 +26,11 @@ ISCSI = 'iscsi'
 
 
 class DetectAlgorithmStorageTransfer(action.Action):
-    def run(self, cloud_src=None, cloud_dst=None, **kwargs):
-        backend_storage_src = cloud_src.resources[utl.STORAGE_RESOURCE].get_backend()
-        backend_storage_dst = cloud_dst.resources[utl.STORAGE_RESOURCE].get_backend()
+    def run(self, cloud_src, cloud_dst, **kwargs):
+        backend_storage_src = cloud_src.resources[
+            utl.STORAGE_RESOURCE].get_backend()
+        backend_storage_dst = cloud_dst.resources[
+            utl.STORAGE_RESOURCE].get_backend()
         res = 0
         if backend_storage_src == ISCSI:
             if backend_storage_dst == ISCSI:
