820f69963b6b9b39fd1d47d3dbf4efb4b0a0fab5
toha <asamoylov@mirantis.com>
2014-11-25 14:29:42 +0300

    Fixed directly instance's volumes transferring

--- cloudferrylib/os/actions/prepare_volumes_data_map.py
+++ cloudferrylib/os/actions/prepare_volumes_data_map.py
@@ -34,26 +34,26 @@ class PrepareVolumesDataMap(action.Action):
         dst_storage_info = copy.deepcopy(dst_vol_info)
         dst_volumes = dst_storage_info[utl.STORAGE_RESOURCE][utl.VOLUMES_TYPE]
 
-        for id, vol in dst_volumes.iteritems():
+        for dst_id, vol in dst_volumes.iteritems():
             src_id = vol[utl.OLD_ID]
             src_host = src_volumes[src_id][utl.VOLUME_BODY]['host']
             src_path = src_volumes[src_id][utl.VOLUME_BODY]['path']
             dst_host = vol[utl.VOLUME_BODY]['host']
             dst_path = vol[utl.VOLUME_BODY]['path']
-            volumes_data_map[id] = {
-                utl.OLD_ID: src_id,
-                utl.VOLUME_BODY: {
-                    utl.SRC_HOST: src_host,
-                    utl.SRC_PATH: src_path,
-                    utl.DST_HOST: dst_host,
-                    utl.DST_PATH: dst_path
-                }
+            volumes_data_map[dst_id] = vol
+            volumes_data_map[dst_id][utl.OLD_ID] = src_id
+            volumes_data_map[dst_id][utl.VOLUME_BODY].update({
+                utl.SRC_HOST: src_host,
+                utl.SRC_PATH: src_path,
+                utl.DST_HOST: dst_host,
+                utl.DST_PATH: dst_path
+            })
+            volumes_data_map[dst_id][utl.META_INFO].update(src_volumes[src_id][utl.META_INFO])
+        volumes = {
+            utl.STORAGE_RESOURCE: {
+                utl.VOLUMES_TYPE: volumes_data_map
             }
-            volumes = {
-                utl.STORAGE_RESOURCE: {
-                    utl.VOLUMES_TYPE: volumes_data_map
-                }
-            }
+        }
 
         return {
             'storage_info': volumes
