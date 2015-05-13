c61355695eef2d7be1f98d2c989d30006a1f2954
Anton Grigoryev <agrigorev@mirantis.com>
2015-05-13 14:15:32 -0700

    Collect hardware info via nova
    Conflicts:
    	condensation/scripts/nova_collector.py

--- condensation/scripts/nova_collector.py
+++ condensation/scripts/nova_collector.py
@@ -17,28 +17,56 @@ import json
 from cloudferrylib.os.compute import nova_compute
 
 
-username = ""
-password = ""
-tenant = ""
-auth_url = ""
+def run_it(conf):
+    """
+    This function is entry point of this Program.
+    The Program read information on source cloud:
+     - about flavors and instances and store it into 'configs/nova.json'
+     - about compute nodes and store it into 'configs/nodes_info.json'
+    """
+    src = conf['src']
+    username = src['user']
+    password = src['password']
+    tenant = src['tenant']
+    auth_url = src['auth_url']
+    dst_comp = conf['dst_compute']
+    core_ratio = dst_comp['cpu_allocation_ratio']
+    ram_ratio = dst_comp['ram_allocation_ratio']
 
-cli = client.Client(2, username, password, tenant, auth_url)
-servers = cli.servers.list(search_opts={"all_tenants": True})
-flavors = cli.flavors.list()
-result = {"vms": {i.id:
-                  {"id": i.id,
-                   "flavor": i.flavor.get("id"),
-                   "host": getattr(i, nova_compute.INSTANCE_HOST_ATTRIBUTE)
-                   } for i in servers
-                  },
-          "flavors": {i.id:
-                      {"fl_id": i.id,
-                          "core": i.vcpus,
-                          "name": i.name,
-                          "ram": i.ram,
-                          "ephemeral": i.ephemeral,
-                          "swap": i.swap
-                       } for i in flavors}}
+    cli = client.Client(2, username, password, tenant, auth_url)
+    servers = cli.servers.list(search_opts={"all_tenants": True})
+    flavors = cli.flavors.list()
+    result = {"vms": {i.id:
+                      {"id": i.id,
+                       "flavor": i.flavor.get("id"),
+                       "host": getattr(i, nova_compute.INSTANCE_HOST_ATTRIBUTE)
+                       } for i in servers
+                      },
+              "flavors": {i.id:
+                          {"fl_id": i.id,
+                              "core": i.vcpus,
+                              "name": i.name,
+                              "ram": i.ram,
+                              "ephemeral": i.ephemeral,
+                              "swap": i.swap
+                           } for i in flavors}}
 
-with open("nova.json", "w") as descriptor:
-    json.dump(result, descriptor)
+    with open("configs/nova.json", "w") as descriptor:
+        json.dump(result, descriptor)
+
+    result_dict = {}
+
+    down_hosts = set([service.host for service in cli.services.findall(
+        binary='nova-compute', state='down')])
+
+    for hypervisor in cli.hypervisors.list():
+        host = hypervisor.hypervisor_hostname
+        if host not in down_hosts:
+            result_dict[host] = {
+                    'core': hypervisor.vcpus,
+                    'ram': hypervisor.memory_mb,
+                    'core_ratio': core_ratio,
+                    'ram_ratio': ram_ratio}
+
+    with open('configs/nodes_info.json', 'w') as nodes_descriptor:
+        json.dump(result_dict, nodes_descriptor)
