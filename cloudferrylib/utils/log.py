7ae5bbdd20b574666ef4434486b460fd9be24350
Sergey Vilgelm <sergey@vilgelm.info>
2015-12-21 16:56:01 -0800

    [CF-106] Rotate log file for every run
    Add RunRotatingFileHandler to rotate logs for every run.

--- cloudferrylib/utils/log.py
+++ cloudferrylib/utils/log.py
@@ -14,6 +14,9 @@
 
 import logging
 from logging import config
+from logging import handlers
+import datetime
+import os
 
 import yaml
 
@@ -23,6 +26,13 @@ getLogger = logging.getLogger
 
 
 def configure_logging():
+    """Configure the logging
+
+    Loading logging configuration file which is defined in the general
+    configuration file and configure the logging sytem.
+    Setting the level of console handler to DEBUG mode if debug option is set
+    as True.
+    """
     with open(cfglib.CONF.migrate.log_config, 'r') as f:
         config.dictConfig(yaml.load(f))
     if cfglib.CONF.migrate.debug:
@@ -31,3 +41,37 @@ def configure_logging():
             if handler.name == 'console':
                 handler.setLevel(logging.DEBUG)
                 break
+
+
+class RunRotatingFileHandler(handlers.RotatingFileHandler):
+    """Handler for logging to switch the logging file every run.
+
+    The handler allows to include the scenario and the current datetime into
+    the filename.
+
+    :param filename: The template for filename
+    :param date_format: The temaplate for formatting the current datetime
+    """
+    def __init__(self,
+                 filename='%(scenario)s-%(date)s.log',
+                 date_format='%F-%H-%M-%S',
+                 **kwargs):
+        self.date_format = date_format
+
+        super(RunRotatingFileHandler, self).__init__(
+                filename=self.get_filename(filename), **kwargs)
+
+    def get_filename(self, filename):
+        """Format the filename
+
+        :param filename: the formatting string for the filename
+        :return: Formatted filename with included scenarion and
+        current datetime.
+        """
+        scenario = os.path.splitext(os.path.basename(
+                cfglib.CONF.migrate.scenario))[0]
+        dt = datetime.datetime.now().strftime(self.date_format)
+        return filename % {
+            'scenario': scenario,
+            'date': dt
+        }
