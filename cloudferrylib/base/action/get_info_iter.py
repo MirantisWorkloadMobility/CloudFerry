4d5c853c46e4eaaa7cb662761a2721107abecc84
Roman Verchikov <roman.verchikov@gmail.com>
2016-01-11 18:44:18 -0800

    CF-89 Show VM progress
    Improves VM migration progress presentation to user.

--- cloudferrylib/base/action/get_info_iter.py
+++ cloudferrylib/base/action/get_info_iter.py
@@ -14,16 +14,73 @@
 
 
 from cloudferrylib.base.action import action
-from cloudferrylib.utils import utils as utl
+from cloudferrylib.utils import utils
+from cloudferrylib.utils import log
+
+
+LOG = log.getLogger(__name__)
+
+
+def get_object_name_from_resource(resource_name):
+    resource_to_object_name_map = {
+        utils.INSTANCES_TYPE: "instance",
+        utils.VOLUMES_TYPE: "volume",
+        utils.NETWORKS_TYPE: "network",
+        utils.IMAGES_TYPE: "image",
+        utils.TENANTS_TYPE: "tenant",
+        utils.USERS_TYPE: "user",
+    }
+
+    return resource_to_object_name_map.get(resource_name, "UNKNOWN OBJECT")
+
+
+class MigrationProgressView(object):
+    """Presents information about object migration process to user"""
+
+    def __init__(self, obj_type, output=LOG):
+        self.type = obj_type
+        self.num = 0
+        self.total = 0
+        self.first_time = True
+        self.log = output
+
+    def show_progress(self, current_object, all_objects):
+        if self.first_time:
+            self.first_time = False
+
+            # one object has been already popped off all_objects
+            # super lame, but still better than messing with GetInfoIter
+            self.total = len(all_objects) + 1
+
+        self.num += 1
+
+        message = ("Starting migration of %(object_type)s '%(name_or_id)s', "
+                   "%(num)d of %(total)d, %(percentage).0f%%")
+        name_or_id = self._get_obj_name(current_object)
+
+        self.log.info(message, {"object_type": self.type,
+                                "name_or_id": name_or_id,
+                                "num": self.num,
+                                "total": self.total,
+                                "percentage": 100.0 * self.num / self.total})
+
+    def _get_obj_name(self, current_object):
+        obj_info = current_object.get(self.type, {})
+        default_name = "NAME OR ID NOT FOUND"
+        return obj_info.get('name') or obj_info.get('id') or default_name
 
 
 class GetInfoIter(action.Action):
 
     def __init__(self, init, iter_info_name='info_iter', info_name='info',
-                 resource_name=utl.INSTANCES_TYPE):
+                 resource_name=utils.INSTANCES_TYPE):
         self.iter_info_name = iter_info_name
         self.info_name = info_name
         self.resource_name = resource_name
+
+        self.view = MigrationProgressView(
+                obj_type=get_object_name_from_resource(resource_name))
+
         super(GetInfoIter, self).__init__(init)
 
     def run(self, **kwargs):
@@ -34,6 +91,8 @@ class GetInfoIter(action.Action):
         new_info = {
             self.resource_name: {obj_id: obj}
         }
+
+        self.view.show_progress(obj, objs)
 
         return {
             self.iter_info_name: info,
