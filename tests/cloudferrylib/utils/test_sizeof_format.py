87ca852f298da4e813ba15d6419f046829c7096b
Sergey Vilgelm <sergey@vilgelm.info>
2016-01-14 15:47:43 -0800

    [CF-86] Fix recreating broken images Refactor ReCreateBootImage. Check that image exists on destination. Create images on destination with keeping old id. Implement unit tests.

--- tests/cloudferrylib/utils/test_sizeof_format.py
+++ tests/cloudferrylib/utils/test_sizeof_format.py
@@ -35,3 +35,34 @@ class SizeOfFormatTestCase(test.TestCase):
 
     def test_fake_current_unit(self):
         self.assertRaises(ValueError, sizeof_format.sizeof_fmt, self.num, 'F')
+
+
+class ParseSizeTestCase(test.TestCase):
+    def parse_size(self, speed_limit=None):
+        return sizeof_format.parse_size(speed_limit)
+
+    def test_none(self):
+        self.assertIsZero(self.parse_size())
+
+    def test_off(self):
+        self.assertIsZero(self.parse_size('off'))
+        self.assertIsZero(self.parse_size('OFF'))
+        self.assertIsZero(self.parse_size('Off'))
+
+    def test_parse(self):
+        for expected_result, value in ((1, '1'), (1, '1b'),
+                                       (1024, '1kb'),
+                                       (1024 * 1024, '1mb'),
+                                       (1024 * 1024 * 1024, '1gb')):
+            result = self.parse_size(value)
+            self.assertEqual(expected_result, result)
+
+    def test_trash(self):
+        self.assertIsZero(self.parse_size('fake_value'))
+
+    def test_int(self):
+        self.assertEqual(123456789, self.parse_size(123456789))
+
+    def test_zero(self):
+        self.assertIsZero(self.parse_size(0))
+        self.assertIsZero(self.parse_size('0'))
