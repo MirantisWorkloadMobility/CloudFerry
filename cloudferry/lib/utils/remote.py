aea9a6389b519e1d16a24960a64b997047cb2ed4
Anton Frolov <frolov.anton@gmail.com>
2016-04-19 18:00:03 -0700

    [CF-426] Add retrying to RemoteExecutor

--- cloudferry/lib/utils/remote.py
+++ cloudferry/lib/utils/remote.py
@@ -14,10 +14,13 @@
 import contextlib
 import logging
 import os
+import socket
 import StringIO
 import sys
 
 import paramiko
+
+from cloudferry.lib.utils import retrying
 
 LOG = logging.getLogger(__name__)
 SHELL = '/bin/bash -c'
@@ -58,18 +61,18 @@ class RemoteExecutor(object):
         settings = self.settings
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(
-            hostname=self.hostname,
-            port=settings.port,
-            username=settings.username,
-            password=settings.password,
-            pkey=self._create_pkey(settings.private_key),
-            sock=self._connect_through_gateway(
-                self.hostname, settings.port, settings.gateway),
-            timeout=self.settings.timeout,
-            allow_agent=False,
-            look_for_keys=False,
-            compress=False)
+        self._retrying(client.connect,
+                       hostname=self.hostname,
+                       port=settings.port,
+                       username=settings.username,
+                       password=settings.password,
+                       pkey=self._create_pkey(settings.private_key),
+                       sock=self._connect_through_gateway(
+                           self.hostname, settings.port, settings.gateway),
+                       timeout=self.settings.timeout,
+                       allow_agent=False,
+                       look_for_keys=False,
+                       compress=False)
         self.connections.append(client)
         self.client = client
 
@@ -114,21 +117,22 @@ class RemoteExecutor(object):
             return None
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(
-            hostname=gateway.hostname,
-            port=gateway.port,
-            username=gateway.username,
-            password=gateway.password,
-            pkey=self._create_pkey(gateway.private_key),
-            sock=self._connect_through_gateway(
-                gateway.hostname, gateway.port, gateway.gateway),
-            timeout=self.settings.timeout,
-            banner_timeout=self.settings.timeout,
-            allow_agent=False,
-            look_for_keys=False,
-            compress=False)
+        self._retrying(client.connect,
+                       hostname=gateway.hostname,
+                       port=gateway.port,
+                       username=gateway.username,
+                       password=gateway.password,
+                       pkey=self._create_pkey(gateway.private_key),
+                       sock=self._connect_through_gateway(
+                           gateway.hostname, gateway.port, gateway.gateway),
+                       timeout=self.settings.timeout,
+                       banner_timeout=self.settings.timeout,
+                       allow_agent=False,
+                       look_for_keys=False,
+                       compress=False)
         self.connections.append(client)
-        return client.get_transport().open_channel(
+        return self._retrying(
+            client.get_transport().open_channel,
             'direct-tcpip', (host, port), ('', 0), window_size=WINDOW_SIZE)
 
     @staticmethod
@@ -147,45 +151,72 @@ class RemoteExecutor(object):
         return None
 
     def _run(self, command, is_sudo):
-        LOG.debug('[%s] running command "%s"', self.hostname, command)
-        if is_sudo:
-            sudo_prompt = '[{magic}] sudo password:'.format(
-                magic=os.urandom(8).encode('hex'))
-            prefix = 'sudo -p \\"{prompt}\\" '.format(prompt=sudo_prompt)
-        else:
-            sudo_prompt = None
-            prefix = ''
-        full_command = '{shell_cmd} "{prefix}{escaped_cmd}"'.format(
-            shell_cmd=SHELL,
-            prefix=prefix,
-            escaped_cmd=self._shell_escape(command))
+        attempts = 0
+        while True:
+            try:
+                LOG.debug('[%s] running command "%s"', self.hostname, command)
+                if is_sudo:
+                    sudo_prompt = '[{magic}] sudo password:'.format(
+                        magic=os.urandom(8).encode('hex'))
+                    prefix = 'sudo -p \\"{prompt}\\" '.format(
+                        prompt=sudo_prompt)
+                else:
+                    sudo_prompt = None
+                    prefix = ''
+                full_command = '{shell_cmd} "{prefix}{escaped_cmd}"'.format(
+                    shell_cmd=SHELL,
+                    prefix=prefix,
+                    escaped_cmd=self._shell_escape(command))
 
-        session = self.client.get_transport().open_session(
-            window_size=WINDOW_SIZE)
-        with session:
-            session.set_combine_stderr(True)
-            session.get_pty()
-            session.settimeout(self.settings.timeout)
-            session.exec_command(full_command)
+                session = self.client.get_transport().open_session(
+                    window_size=WINDOW_SIZE)
+                with session:
+                    session.set_combine_stderr(True)
+                    session.get_pty()
+                    session.settimeout(self.settings.timeout)
+                    session.exec_command(full_command)
 
-            output = ''
-            while not session.closed or session.recv_ready():
-                output += session.recv(1)
-                if sudo_prompt and output.endswith(sudo_prompt):
-                    session.sendall(self.settings.password + '\n')
-                    output = output[:-len(sudo_prompt)]
+                    output = ''
+                    while not session.closed or session.recv_ready():
+                        output += session.recv(1)
+                        if sudo_prompt and output.endswith(sudo_prompt):
+                            session.sendall(self.settings.password + '\n')
+                            output = output[:-len(sudo_prompt)]
 
-            if not self.ignore_errors and session.recv_exit_status() != 0:
-                message = 'Error running command on host {host}: {cmd}'.format(
-                    host=self.hostname, cmd=command)
-                LOG.debug('Error running command %s on host %s: %s',
-                          full_command, self.hostname, output)
-                raise RemoteFailure(message, output, full_command)
+                    if not self.ignore_errors \
+                            and session.recv_exit_status() != 0:
+                        message = ('Error running command on host {host}: '
+                                   '{cmd}').format(
+                                       host=self.hostname, cmd=command)
+                        LOG.debug('Error running command %s on host %s: %s',
+                                  full_command, self.hostname, output)
+                        raise RemoteFailure(message, output, full_command)
 
-        return output
+                return output
+            except (paramiko.SSHException, socket.timeout):
+                attempts += 1
+                LOG.debug('Failed to execute command %s', command,
+                          exc_info=True)
+                if attempts >= self.settings.connection_attempts:
+                    raise
+                self._reconnect()
 
     @staticmethod
     def _shell_escape(str_value):
         for ch in ('$', '"', '`'):
             str_value = str_value.replace(ch, '\\' + ch)
         return str_value
+
+    def _retrying(self, func, *args, **kwargs):
+        retry = retrying.Retry(
+            max_attempts=self.settings.connection_attempts,
+            timeout=self.settings.attempt_failure_sleep,
+            reraise_original_exception=True,
+            expected_exceptions=(paramiko.BadHostKeyException,
+                                 paramiko.AuthenticationException,
+                                 RemoteFailure))
+        return retry.run(func, *args, **kwargs)
+
+    def _reconnect(self):
+        self.close()
+        self.connect()
